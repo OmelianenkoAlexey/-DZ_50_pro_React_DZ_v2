c2b942d546e3b85b3eef90c65890330af3646c04
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-03-17 12:54:08 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Grid, Box, styled } from '@mui/material';
+import './App.css';
+import Header from './components/Templates/Header';
+import NavBar from './components/Templates/NavBar';
+import Main from './components/Templates/Main';
+import Footer from './components/Templates/Footer';
+
+
+const HeaderWrapper = styled(Box)(() => ({
+  backgroundColor: "blue",
+  height: "15vh",
+  display: "flex",
+  justifyContent: "center",
+  padding: "20px",
+}));
+
+const NavBarWrapper = styled(Box)(() => ({
+  backgroundColor: "green",
+  height: "70vh",
+
+}));
+
+const MainWrapper = styled(Box)(() => ({
+  backgroundColor: "red",
+  height: "70vh",
+  padding: "20px",
+}));
+
+const FooterWrapper = styled(Box)(() => ({
+  backgroundColor: "yellow",
+  height: "15vh",
+  display: "flex",
+  justifyContent: "center",
+}));
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Grid container>
+        <Grid item xs={12} md={12} lg={12}>
+          <HeaderWrapper>
+            <Header />
+          </HeaderWrapper>
+        </Grid>
+        <Grid item sm={3} md={3} lg={3} display={{xs: "none", sm: "block"}}>
+          <NavBarWrapper>
+            <NavBar />
+          </NavBarWrapper>
+        </Grid>
+        <Grid item xs={12} sm={9} md={9} lg={9}>
+          <MainWrapper>
+            <Main />
+          </MainWrapper>
+        </Grid>
+        <Grid item xs={12} md={12} lg={12}>
+          <FooterWrapper>
+            <Footer />
+          </FooterWrapper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Templates/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography } from '@mui/material';
+
+export default function Header() {
+
+    return (
+        <>
+            <Typography
+                variant="h1"
+                component="h2"
+                color="white"
+            >
+                My React project
+            </Typography>;
+        </>
+    )
+}
