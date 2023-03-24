f15719066ee0ee86f87060635f756ee47d1b84a1
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2023-03-24 17:10:41 +0200

    yarn install

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create ({
-    baseURL:"https://63f0c7635b7cf4107e26a8c8.mockapi.io/api/v1",
-    headers: { 'Content-Type': "application/json" },
+    baseURL:'https://63f0c7635b7cf4107e26a8c8.mockapi.io/api/v1',
+    headers: { 'Content-Type': 'application/json' },
 });
